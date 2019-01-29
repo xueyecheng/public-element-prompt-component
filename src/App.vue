cac78f91905e21bb552aaed331630586c9428397
xueyecheng <cheng.xueye@ycgame.com>
2019-01-29 16:56:18 +0800

    添加项目相关文件

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+	<div id="app">
+		<el-button @click="showComponent" type="danger" plain>删除</el-button>
+		<publicComponent :msgConfig="publicComponentObj.msgConfig" :confirmConfig="publicComponentObj.confirmConfig"></publicComponent>
+	</div>
+</template>
+
+<script>
+import PublicComponent from './component/PublicComponent'
+export default {
+	name: 'app',
+	data() {
+		return {
+			msg: 'Welcome to Your Vue.js App',
+			publicComponentObj: {
+				confirmConfig: {},
+				msgConfig: {},
+			}
+		}
+	},
+	methods: {
+		showComponent() {
+			const tempObj = {
+				message: `确认移出选中成员吗？`,
+				title: '提示',
+				options: {
+					type: 'warning'
+				},
+				yesCallback: () => {
+					const tempMsgObj = {
+						message: '删除成功',
+						type: 'success',
+						showClose: true,
+						duration: 3000
+					}
+					this.publicComponentObj.msgConfig = tempMsgObj;
+				}
+			}
+			this.publicComponentObj.confirmConfig = tempObj;
+		}
+	},
+	components: {
+		PublicComponent
+	}
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/component/PublicComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="PublicComponent"></div>
+</template>
+
+<script>
+export default {
+    name: 'PublicComponent',
+    data() {
+        return {
+
+        }
+    },
+    props:{
+        msgConfig:Object,
+        confirmConfig:Object,
+        promptConfig:Object
+    },
+    watch: {
+        msgConfig: function (val, oldVal) {
+            if (val != oldVal) {
+                this.showMassage(val);
+            }
+        },
+        confirmConfig: function (val, oldVal) {
+            if (val != oldVal) {
+                this.showConfirmModal(val);
+            }
+        },
+        promptConfig: function (val, oldVal) {
+            if (val != oldVal) {
+                this.showPromptModal(val);
+            }
+        }
+    },
+    methods: {
+        showMassage(config) {
+            this.$message(config);
+        },
+        showConfirmModal(config) {
+            this.$confirm(config.message, config.title, config.options ? config.options : {}).then(config.yesCallback).catch(config.cancelCallback ? config.cancelCallback : () => { });
+        },
+        showPromptModal(config) {
+            this.$prompt(config.message, config.title, config.options ? config.options : {}).then(config.yesCallback).catch(config.cancelCallback ? config.cancelCallback : () => { });
+        }
+    }
+}
+</script>
